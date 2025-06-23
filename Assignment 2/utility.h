9000088a65851c3894a7ea45c6d05e1881040292
Pedram P <pedram.pasandide1993@gmail.com>
2025-06-23 15:04:31 -0400

    start with

--- Assignment 2/utility.h
+++
@@ -1,6 +0,0 @@
-#ifndef UTILITY_H
-#define UTILITY_H
-
-// CODE: the declarations [only the necessary ones] of functiona and related variables 
-
-#endif
--- Assignment 3/utility.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILITY_H
-#define UTILITY_H
-
-// CODE: declare any function implemented in utility (if you need more)
-void fillRandom(double **matrix, int rows, int cols);
-void printMatrix(double **matrix, int rows, int cols);
-
-
-#endif
